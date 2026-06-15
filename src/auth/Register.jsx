382cb41081b03cc89305a0c4e18ac58a7c8e8906
AOrnstein <ornstein.adam@gmail.com>
2026-06-15 03:09:27 -0600

    add login and regester pages

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,3 +1,54 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router";
+
+import { useAuth } from "./AuthContext";
+
+/** A form that allows users to register for a new account */
 export default function Register() {
-  return <p>Register Page</p>;
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const [error, setError] = useState(null);
+
+  const tryRegister = async (formData) => {
+    setError(null);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const firstname = formData.get("firstname");
+    const lastname = formData.get("lastname");
+    try {
+      await register(email, password, firstname, lastname);
+      navigate("/books");
+    } catch (e) {
+      setError(e.message);
+    }
+  };
+
+  return (
+    <>
+      <h1>Register for an account</h1>
+      <form action={tryRegister}>
+        <label>
+          First Name
+          <input type="text" name="firstname" />
+        </label>
+        <label>
+          Last Name
+          <input type="text" name="lastname" />
+        </label>
+        <label>
+          Email
+          <input type="text" name="email" required />
+        </label>
+        <label>
+          Password
+          <input type="password" name="password" required />
+        </label>
+        <button>Register</button>
+        {error && <p role="alert">{error}</p>}
+      </form>
+      <Link to="/login">Already have an account? Log in here.</Link>
+    </>
+  );
 }
